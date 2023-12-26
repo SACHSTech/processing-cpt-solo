07187b34b563c2f6d153853c7a7d7447a03be65b
joshu2761 <joshua.y0761@gmail.com>
2023-12-26 15:30:59 -0500

    Created Title Screen Made movement and collision

--- Main.java
+++ Main.java
@@ -9,11 +9,14 @@ class Main {
   public static void main(String[] args) {
     
     String[] processingArgs = {"MySketch"};
-	  Sketch mySketch = new Sketch();  //comment this out to run the other sketch files
-	  // Sketch1 mySketch = new Sketch1();  // uncomment this to run this sketch file
-	  // Sketch2 mySketch = new Sketch2();  // uncomment this to run this sketch file
-	  
-	  PApplet.runSketch(processingArgs, mySketch);
+
+	
+	Sketch mySketch = new Sketch();
+	
+	// Sketch1 mySketch = new Sketch1();  // uncomment this to run this sketch file
+	// Sketch2 mySketch = new Sketch2();  // uncomment this to run this sketch file
+	
+	PApplet.runSketch(processingArgs, mySketch);
   }
   
 }
--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,208 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
-
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
-
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
+  PImage imgTitleImage;
+  PImage imgBackground;
+  PImage imgBackgroundHitBoxes;
+  PImage imgPlayer;
+
+  boolean playGame;
+
+  // Location of the player on the Map
+  float playerXMap;
+  float playerYMap;
+  // Location of the player on the Screen
+  float playerXScreen;
+  float playerYScreen;
+  float playerSpeedX;
+  float playerSpeedY;
+  float mapX;
+  float mapY;
+  boolean isColliding;
+
+  public void settings() {
+    size(768, 768);
+  }
+
+  public void setup() {
+
+    imgTitleImage = loadImage("TitleScreen.png");
+    imgBackground = loadImage("Background.png");
+    imgBackgroundHitBoxes = loadImage("BackgroundHitBoxes.png");
+
+    imgPlayer = loadImage("PlayerWeapon1.png");
+
+    imgBackground.resize(imgBackground.width / 4 * 3, imgBackground.height / 4 * 3);
+    imgBackgroundHitBoxes.resize(imgBackgroundHitBoxes.width / 4 * 3, imgBackgroundHitBoxes.height / 4 * 3);
+    imgPlayer.resize(imgPlayer.width / 4 * 3, imgPlayer.height / 4 * 3);
+    imgPlayer.resize(imgPlayer.width / 5, imgPlayer.height / 5);
+
+    playGame = false;
+
+    playerXMap = -(imgBackground.width / 2 - imgPlayer.width / 2);
+    playerYMap = -(imgBackground.height / 2 - imgPlayer.height / 2);  
+  }
+
+  public void draw() {
+
+    if (!playGame) {
+      background(imgTitleImage);
+    }
+    else {
+      collision();
+
+      // Print playerX and playerY at the bottom right of the application
+      textAlign(RIGHT, BOTTOM);
+      textSize(16);
+      fill(0);
+      text("playerX: " + playerXMap + ", playerY: " + playerYMap, width, height);
+      text("playerWidth: " + imgPlayer.width + ", playerHeight: " + imgPlayer.height, width, height - 16);
+      text("MapSizeX: " + imgBackground.width + ", MapSizeY: " + imgBackground.height, width, height - 32);
+      text("ScreenSizeX: " + width + ", ScreenSizeY: " + height, width, height - 48);
+    }
+  }
+
+  /*
+   * Collision detection and moving the map around the player.
+   * @author: Joshua Yin
+   * @param none
+   * @return void
+   */
+  public void collision() {
+
+    // If the Player gets close to the edge of the map, the player moves instead of the map
+    if (playerXMap > -width / 2) {
+      mapX = 0;
+      mapY = playerYMap + height / 2;
+      playerXScreen = -playerXMap - imgPlayer.width / 2;
+      playerYScreen = height / 2 - imgPlayer.height / 2;
+    }
+    else if (playerXMap < -(imgBackground.width - width / 2)) {
+      mapX = -imgBackground.width + width;
+      mapY = playerYMap + height / 2;
+      playerXScreen = width - imgPlayer.width / 2 - (imgBackground.width + playerXMap);
+      playerYScreen = height / 2 - imgPlayer.height / 2;
+    }
+    else if (playerYMap > -height / 2) {
+      mapX = playerXMap + width / 2;
+      mapY = 0;
+      playerXScreen = width / 2 - imgPlayer.width / 2;
+      playerYScreen = -playerYMap - imgPlayer.height / 2;
+    }
+    else if (playerYMap < -(imgBackground.height - height / 2)) {
+      mapX = playerXMap + width / 2;
+      mapY = -imgBackground.height + height;
+      playerXScreen = width / 2 - imgPlayer.width / 2;
+      playerYScreen = height - imgPlayer.height / 2 - (imgBackground.height + playerYMap);
+    }
+
+    // Otherwise, the player stays in the center of the screen and the map moves around the player
+    else {
+      mapX = playerXMap + width / 2;
+      mapY = playerYMap + height / 2;
+      playerXScreen = width / 2 - imgPlayer.width / 2;
+      playerYScreen = height / 2 - imgPlayer.height / 2;
+    }
+
+    // Draws the hitbox map
+    image(imgBackgroundHitBoxes, mapX, mapY);
+
+    // Collision detection
+    // If the player goes to the edge of the map, the player stops moving
+    if (playerXMap > 0) {
+      isColliding = true;
+      playerXMap = 0;
+    }
+    else if (playerXMap < -(imgBackground.width)) {
+      playerXMap = -(imgBackground.width);
+    }
+    else if (playerYMap > 0) {
+      playerYMap = 0;
+    }
+    else if (playerYMap < -(imgBackground.height)) {
+      playerYMap = -(imgBackground.height);
+    }
+
+    // If the player collides with a black pixel, they stop moving
+
+    else if (get((int)playerXScreen, (int)playerYScreen + imgPlayer.height / 2) == color(0, 0, 0)) {
+      if (playerSpeedX > 0) {
+        playerXMap -= playerSpeedX;
+      }
+      if (playerSpeedY > 0) {
+        playerYMap -= playerSpeedY;
+      }
+    }
+    else if (get((int)playerXScreen + imgPlayer.width, (int)playerYScreen + imgPlayer.height / 2) == color(0, 0, 0)) {
+      if (playerSpeedX < 0) {
+        playerXMap -= playerSpeedX;
+      }
+      if (playerSpeedY > 0) {
+        playerYMap -= playerSpeedY;
+      }
+    }
+    else if (get((int)playerXScreen, (int)playerYScreen + imgPlayer.height) == color(0, 0, 0)) {
+      if (playerSpeedX > 0) {
+        playerXMap -= playerSpeedX;
+      }
+      if (playerSpeedY < 0) {
+        playerYMap -= playerSpeedY;
+      }
+    }
+    else if (get((int)playerXScreen + imgPlayer.width, (int)playerYScreen + imgPlayer.height) == color(0, 0, 0)) {
+      if (playerSpeedX < 0) {
+        playerXMap -= playerSpeedX;
+      }
+      if (playerSpeedY < 0) {
+        playerYMap -= playerSpeedY;
+      }
+    }
+    
+    // Moves the player
+    playerXMap += playerSpeedX;
+    playerYMap += playerSpeedY;
+
+    // Draws the real map over the hitbox map
+    image(imgBackground, mapX, mapY);
+    // Draws the player
+    image(imgPlayer, playerXScreen, playerYScreen);
+  }
+
+  public void keyPressed() {
+    
+    if (playGame == false) {
+
+      playGame = true;
+    }
+    
+    if (key == 'w') {
+      playerSpeedY = 5;
+    }
+    else if (key == 'a') {
+      playerSpeedX = 5;
+    }
+    else if (key == 's') {
+      playerSpeedY = -5;
+    }
+    else if (key == 'd') {
+      playerSpeedX = -5;
+    }
+  }
+  public void keyReleased() {
+    
+    if (key == 'w') {
+      playerSpeedY = 0;
+    }
+    else if (key == 'a') {
+      playerSpeedX = 0;
+    }
+    else if (key == 's') {
+      playerSpeedY = 0;
+    }
+    else if (key == 'd') {
+      playerSpeedX = 0;
+    }
+  }
 }
